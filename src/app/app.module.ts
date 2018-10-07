3b2e5b3645f1212906f2fdae1175c7b66d1fa003
Paula Quitián <pqhernandez@gmail.com>
2018-10-07 15:06:02 +0200

    feat: add angular sui module :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {SuiModule} from 'ng2-semantic-ui';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    SuiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
