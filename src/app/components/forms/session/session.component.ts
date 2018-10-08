7b08db991d3339e0868226552bc085343cfc8244
Paula Quitián <pqhernandez@gmail.com>
2018-10-08 23:16:22 +0200

    feat: add session component working

--- src/app/components/forms/session/session.component.ts
+++ src/app/components/forms/session/session.component.ts
@@ -1,15 +1,32 @@
+import { ApiErrors } from './../../../shared/models/api-errors.model';
+import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
+import { SessionService } from './../../../shared/services/session.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../../shared/models/user.model';
 
 @Component({
   selector: 'app-session',
   templateUrl: './session.component.html',
   styleUrls: ['./session.component.css']
 })
-export class SessionComponent implements OnInit {
+export class SessionComponent {
+  
+  user: User = new User();
+  apiError: ApiErrors;
 
-  constructor() { }
+  constructor(private sessionService: SessionService, private router: Router) { }
 
-  ngOnInit() {
+  onSubmitLogIn(sessionForm: FormGroup): void {
+    if (sessionForm.valid) {
+      this.sessionService.authenticate(this.user)
+        .subscribe(() => {
+          sessionForm.reset();
+          this.router.navigate(['search']);
+        },
+          (error: ApiErrors) => this.apiError
+        );
+    }
   }
 
 }
