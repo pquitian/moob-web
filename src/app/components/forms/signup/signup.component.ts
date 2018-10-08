c83c0fbb85c683515f5239a518e8709054a8b55a
Paula Quitián <pqhernandez@gmail.com>
2018-10-08 21:26:19 +0200

    feat: add session component

---
+++ src/app/components/forms/signup/signup.component.ts
@@ -0,0 +1,33 @@
+import { ApiErrors } from './../../../shared/models/api-errors.model';
+import { Component } from '@angular/core';
+import { UserService } from '../../../shared/services/user.service';
+import { FormGroup } from '@angular/forms';
+import { User } from '../../../shared/models/user.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+
+  user: User = new User();
+  ApiErrors: ApiErrors;
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  onSubmitSignUp(signupForm: FormGroup): void {
+    console.log(signupForm);
+    if (signupForm.valid) {
+      this.userService.create(this.user)
+        .subscribe(()=> {
+          signupForm.reset();
+          this.router.navigate(['/login']);
+        }, (error: ApiErrors) => this.ApiErrors = error
+      );
+    }
+
+  }
+
+}
