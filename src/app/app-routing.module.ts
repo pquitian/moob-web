31db7f87f8a809eb0194443a2fe450e5da6c2869
Paula Quitián <pqhernandez@gmail.com>
2018-10-09 18:38:45 +0200

    feat: add main component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './components/forms/signup/signup.component';
+import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: SessionComponent },
-  { path: 'search', component: HomeComponent }
+  { path: 'search', component: MainComponent }
 ];
 
 @NgModule({
