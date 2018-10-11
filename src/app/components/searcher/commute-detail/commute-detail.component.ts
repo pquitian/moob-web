fc98c9eb7676bce65ac077ec58079963ca48036c
Paula Quitián <pqhernandez@gmail.com>
2018-10-11 17:24:30 +0200

    feat: add commute detail component

---
+++ src/app/components/searcher/commute-detail/commute-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-commute-detail',
+  templateUrl: './commute-detail.component.html',
+  styleUrls: ['./commute-detail.component.css']
+})
+export class CommuteDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
