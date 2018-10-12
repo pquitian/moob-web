8f242f9cebfe197b990c9a8dfb713ffb8857f50c
Paula Quitián <pqhernandez@gmail.com>
2018-10-13 00:51:36 +0200

    feat: add vehicles service with create method

---
+++ src/app/shared/services/vehicles.service.ts
@@ -0,0 +1,51 @@
+import { map, catchError } from 'rxjs/operators';
+import { User } from './../models/user.model';
+import { ApiErrors } from './../models/api-errors.model';
+import { Observable, Subject } from 'rxjs';
+import { Vehicle } from './../models/vehicle.model';
+import { HttpClient } from '@angular/common/http';
+import { BaseApiService } from './base-api.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VehiclesService extends BaseApiService {
+
+  private static readonly API_USERS = `${BaseApiService}/users`;
+  private static readonly API_VEHICLES = '/vehicles';
+
+  vehicles: Vehicle[] = [];
+  vehiclesSubject: Subject<Vehicle[]> = new Subject();
+
+  constructor(
+    private http: HttpClient
+  ) {
+    super();
+  }
+
+  create(userId: string, vehicle: Vehicle): Observable<Vehicle | ApiErrors> {
+    return this.http.post<Vehicle>(
+      `${VehiclesService.API_USERS}/${userId}${VehiclesService.API_VEHICLES}`,
+      vehicle, 
+      BaseApiService.defaultOptions
+    ).pipe(
+      map((vehicle: Vehicle) => {
+        vehicle = Object.assign(new Vehicle(), vehicle);
+        this.vehicles.push(vehicle);
+        this.notifyVehiclesChanges();
+        return vehicle;
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  onVehiclesChanges(): Observable <Array<Vehicle>> {
+    return this.vehiclesSubject.asObservable();
+  }
+
+  private notifyVehiclesChanges(): void {
+    this.vehiclesSubject.next(this.vehicles);
+  }
+
+}
