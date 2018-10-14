fcb5759eef6704eee2359999e9ff530bfc74f76d
Paula Quitián <pqhernandez@gmail.com>
2018-10-14 13:41:08 +0200

    fix: create commute correctly :)

--- src/app/components/forms/create-commute/create-commute.component.ts
+++ src/app/components/forms/create-commute/create-commute.component.ts
@@ -1,3 +1,4 @@
+import { GeoService } from './../../../shared/services/geo.service';
 import { VehiclesService } from './../../../shared/services/vehicles.service';
 import { Vehicle } from './../../../shared/models/vehicle.model';
 import { Subscription } from 'rxjs';
@@ -6,9 +7,10 @@ import { User } from './../../../shared/models/user.model';
 import { Router } from '@angular/router';
 import { CommutesService } from './../../../shared/services/commutes.service';
 import { Commute } from './../../../shared/models/commute.model';
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { CreateVehicleComponent } from '../create-vehicle/create-vehicle.component';
+import { Coordinates } from 'src/app/shared/models/coordinates.model';
 
 @Component({
   selector: 'app-create-commute',
@@ -25,11 +27,21 @@ export class CreateCommuteComponent implements OnInit, OnDestroy {
   @ViewChild(CreateVehicleComponent) createVehicle: CreateVehicleComponent;
   @ViewChild('createCommuteForm') createCommuteForm: FormGroup;
 
+  searchControl: FormControl;
+  @ViewChild('origin') originInput: ElementRef;
+  origin: Coordinates = new Coordinates();
+  originChangesSubscription: Subscription;
+
+  @ViewChild('destination') destinationInput: ElementRef;
+  destination: Coordinates = new Coordinates();
+  destinationChangesSubscription: Subscription;
+
   constructor(
     private commutesService: CommutesService,
     private router: Router,
-    private sessionService: SessionService, 
-    private vehiclesService: VehiclesService
+    private sessionService: SessionService,
+    private vehiclesService: VehiclesService,
+    private geoService: GeoService
   ) { }
 
   ngOnInit() {
@@ -42,15 +54,31 @@ export class CreateCommuteComponent implements OnInit, OnDestroy {
     this.onAuthUserChanges = this.sessionService.onUserChanges()
     .subscribe((user: User) => this.authUser = user);
 
+    this.searchControl = new FormControl();
+    this.geoService.setOrigin(this.originInput);
+    this.originChangesSubscription = this.geoService.onOriginChanges()
+      .subscribe((origin: Coordinates) => this.origin = origin );
+
+    this.geoService.setDestination(this.destinationInput);
+    this.destinationChangesSubscription = this.geoService.onDestinationChanges()
+        .subscribe((destination: Coordinates) => this.destination = destination );
+
   }
 
   ngOnDestroy() {
     this.onAuthUserChanges.unsubscribe();
+    this.originChangesSubscription.unsubscribe();
+    this.destinationChangesSubscription.unsubscribe();
   }
 
   onSubmitCreateCommute(commuteForm: FormGroup) {
     console.log(this.createVehicle);
     console.log(commuteForm);
+    this.commute.origin[0] = this.origin.lat;
+    this.commute.origin[1] = this.origin.lng;
+    this.commute.destination[0] = this.destination.lat;
+    this.commute.destination[1] = this.destination.lng;
+    console.log(this.commute);
     if (commuteForm.valid && this.isVehicleFormValid) {
       this.vehiclesService.create(this.authUser.id, this.createVehicle.vehicle)
       .subscribe((vehicle: Vehicle) => {
@@ -60,6 +88,18 @@ export class CreateCommuteComponent implements OnInit, OnDestroy {
           this.router.navigate(['/commutes', this.commute.id]);
         });
       });
+    }
+
+    if (commuteForm.valid && this.commute.vehicle) {
+      console.log('HOLA PAULA');
+      console.log('This commute -->', this.commute);
+      console.log('This commute vehicle --> ', this.commute.vehicle);
+      console.log('This commute vehicle id --> ', this.commute.vehicle.id);
+      this.commutesService.createCommute(this.commute)
+        .subscribe(() => {
+          commuteForm.reset();
+          this.router.navigate(['/commutes', this.commute.id]);
+        });
     }
   }
 
