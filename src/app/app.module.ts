92c523e20cba6c29314be35100f801363f8fc709
Paula Quitián <pqhernandez@gmail.com>
2018-10-17 22:46:34 +0200

    chore: remove pipe from app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,7 +23,7 @@ import { AgmCoreModule } from '@agm/core';
 import { AgmDirectionModule } from 'agm-direction';
 import { CreateCommuteComponent } from './components/forms/create-commute/create-commute.component';
 import { CreateVehicleComponent } from './components/forms/create-vehicle/create-vehicle.component';
-import { AddressPipe } from './shared/pipes/address.pipe';
+// import { AddressPipe } from './shared/pipes/address.pipe';
 import { ChatroomComponent } from './components/chat/chatroom/chatroom.component';
 
 @NgModule({
@@ -40,7 +40,7 @@ import { ChatroomComponent } from './components/chat/chatroom/chatroom.component
     MapComponent,
     CreateCommuteComponent,
     CreateVehicleComponent,
-    AddressPipe,
+    // AddressPipe,
     ChatroomComponent
   ],
   imports: [
